537d138da54247d010e5105266fab60081bfd8ce
Niander Brox <vivekkumar.vk0137@gmail.com>
2025-12-08 09:46:31 +0530

    Express Config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/db.js";
+import { app } from './app.js';
 
 dotenv.config({ path : ".env"})
 
 connectDB()
+.then(()=>{
+    //Application Listening
+    const port = process.env.PORT || 8000
+    app.listen(port, ()=>{
+        console.log(`Server is running at port: ${port}`);
+    })
+
+    //Error Listen
+    app.on("error",(error)=>{
+        console.log("Error: ", error);
+        throw error;
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection Failed !!! ", err);
+})
